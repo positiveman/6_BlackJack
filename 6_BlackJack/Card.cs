f4451fd560b382354dd6e578b15c9b553f765a49
avdeevoleg91 <avdeevoleg91@gmail.com>
2017-12-28 01:44:13 +0200

    ver.1 -Computer and player are in the same conditions, they don't see cards of each other. -Just "one game mode", without result-counter.

---
+++ 6_BlackJack/Card.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_BlackJack
+{
+    class Card
+    {
+        public string _Value {get; private set; }
+        public string _Suit { get; private set; }
+        public int _Points { get; private set; }
+
+        public Card(string value, string suit)
+        {
+            _Value = value;
+            _Suit = suit;
+            if (value=="Ace")
+            {
+                _Points = 11;
+            }
+            else if (value == "King")
+            {
+                _Points = 4;
+            }
+            else if (value == "Queen")
+            {
+                _Points = 3;
+            }
+            else if (value == "Jack")
+            {
+                _Points = 2;
+            }
+            else if (value == "10")
+            {
+                _Points = 10;
+            }
+            else if (value == "9")
+            {
+                _Points = 9;
+            }
+            else if (value == "8")
+            {
+                _Points = 8;
+            }
+            else if (value == "7")
+            {
+                _Points = 7;
+            }
+            else if (value == "6")
+            {
+                _Points = 6;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return _Value + " " + _Suit;
+        }
+    }
+}
--- 6_BlackJack/Program.cs
+++ 6_BlackJack/Program.cs
@@ -10,6 +10,182 @@ namespace _6_BlackJack
     {
         static void Main(string[] args)
         {
+            //Create the card deck
+
+
+            List<String> Values = new List<String> { 6.ToString(), 7.ToString(), 8.ToString(), 9.ToString(), 10.ToString(), "Jack", "Queen", "King", "Ace" };
+            List<String> Suits = new List<String> { "Spades", "Clubs", "Diamonds", "Hearts" };
+
+            List<Card> CardDeck = new List<Card> { };
+
+
+            for (int i = 0; i < Values.Count; i++)
+            {
+                for (int j = 0; j < Suits.Count; j++)
+                {
+                    CardDeck.Add(new Card(Values[i], Suits[j]));
+                }
+
+
+            }
+
+
+
+            //Radomiser for random card giving.
+            Random r = new Random();
+
+            //Lists of computer's and player's cards
+
+            List<Card> ComputerCards = new List<Card> { };
+            List<Card> PlayerCards = new List<Card> { };
+
+
+
+            //Game logic
+
+
+            while (true)
+            {
+                int ComputerPoints = 0;
+                int PlayerPoints = 0;
+
+                //Computer and player get 2 cards 
+                for (int i = 0; i < 2; i++)
+                {
+                    int ComputerCard = r.Next(CardDeck.Count - 1);
+                    ComputerCards.Add(CardDeck[ComputerCard]);
+                    ComputerPoints += CardDeck[ComputerCard]._Points;
+                    CardDeck.RemoveAt(ComputerCard);
+
+
+
+                    int PlayerCard = r.Next(CardDeck.Count - 1);
+                    PlayerCards.Add(CardDeck[PlayerCard]);
+                    PlayerPoints += CardDeck[PlayerCard]._Points;
+                    CardDeck.RemoveAt(PlayerCard);
+
+
+                }
+                foreach (Card card in PlayerCards)
+                {
+                    Console.WriteLine("Your card: " + card.ToString());
+
+                }
+                Console.WriteLine("You have " + PlayerPoints + " points");
+
+
+                if (ComputerPoints == 22)
+                {
+                    Console.WriteLine("Computer WON!");
+                    break;
+
+                }
+                if (PlayerPoints == 22)
+                {
+                    Console.WriteLine("You WON!");
+                    break;
+
+                }
+
+
+                //  Card by card for player
+
+                while (true)
+                {
+
+                    Console.WriteLine("One more card? (Y)es or (N)o");
+                    string answer = Console.ReadLine();
+
+                    if (answer == "y")
+                    {
+                        int PlayerCard = r.Next(CardDeck.Count - 1);
+                        PlayerCards.Add(CardDeck[PlayerCard]);
+                        PlayerPoints += CardDeck[PlayerCard]._Points;
+                        CardDeck.RemoveAt(PlayerCard);
+
+                        Console.WriteLine("Your card: " + PlayerCards.Last().ToString());
+
+
+                        Console.WriteLine("You have " + PlayerPoints + " points");
+
+                        if (PlayerPoints > 21)
+
+                        {
+                            break;
+                        }
+
+                    }
+
+                    if (answer == "n")
+                    {
+                        break;
+                    }
+
+                }
+
+
+                //  Card by card for computer
+
+
+                while (ComputerPoints < 17)
+                {
+                    int ComputerCard = r.Next(CardDeck.Count - 1);
+                    ComputerCards.Add(CardDeck[ComputerCard]);
+                    ComputerPoints += CardDeck[ComputerCard]._Points;
+                    CardDeck.RemoveAt(ComputerCard);
+
+                }
+
+                Console.WriteLine("Computer has {0} points", ComputerPoints);
+
+                //Result of GAME!!!
+
+                if (ComputerPoints > PlayerPoints)
+                {
+                    if (ComputerPoints > 21)
+                    {
+                        Console.WriteLine("You won!");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Computer won!");
+
+                    }
+
+
+                }
+
+                else if (PlayerPoints > ComputerPoints)
+                {
+                    if (PlayerPoints > 21)
+                    {
+                        Console.WriteLine("Computer won!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You won!");
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine("It's a draw!");
+                }
+
+                break;
+
+
+
+            }
+
+
+
+
+
         }
+
+
     }
 }
